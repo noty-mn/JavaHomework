414643f12bbf84c3b9c0f94c2984adb4850c2677
noty-mn <71215517+noty-mn@users.noreply.github.com>
2022-04-18 11:27:21 +0800

    asd

--- Login/post.js
+++ Login/post.js
@@ -1,9 +1,20 @@
 var button = document.getElementById('submit');
 var post = document.getElementById('post');
 var targetfield = document.getElementById('targetfield');
-var view = document.querySelector('view');
+var view = document.getElementById('view-cont');
 
-button.addEventListener('click', function(){
-    var post_text = post.value;
-    targetfield.innerHTML = post_text;
-})
+// button.addEventListener('click', function(){
+//     // var post_text = post.value;
+//     // targetfield.innerHTML = post_text;
+//     var poster = document.createElement('div')
+//     poster.id = 'targetfield'
+//     view.appendChild(poster)
+// })
+
+function newpost () {
+    var poster = document.createElement('div')
+    poster.id = 'targetfield'
+    poster.innerText = post.value
+    view.appendChild(poster)
+}
+var localpost = post.value
